8cd1849d662e8caa595cdb96fa0730a68dd4a23b
Quan Nguyen <huu2uan@gmail.com>
2019-07-26 21:45:27 +0700

    sample app

---
+++ sidecar/clj-web/src/clj_web/handler.clj
@@ -0,0 +1,12 @@
+(ns clj-web.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+
+(defroutes app-routes
+  (GET "/" [] "Hello World")
+  (GET "/admin" [] "Admin Page")
+  (route/not-found "Not Found"))
+
+(def app
+  (wrap-defaults app-routes site-defaults))
